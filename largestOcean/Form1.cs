5a5f3f4856a4c4e20c65df64462bae0f42634573
JC <blahblahblah@email.com>
2020-03-05 10:20:31 -0600

    Add project files.

---
+++ largestOcean/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace largestOcean
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCheckAnswer_Click(object sender, EventArgs e)
+        {
+            string answer = txtAnswer.Text;
+            answer = answer.ToLower();
+            if (answer == "pacific")
+            {
+                MessageBox.Show("Correct!", "Result");
+            }
+            else
+            {
+                MessageBox.Show("Incorrect, the answer is the \"Pacific\" Ocean", "Result");
+            }
+        }
+    }
+}
